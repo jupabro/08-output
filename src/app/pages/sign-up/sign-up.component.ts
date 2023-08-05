13fd0866c0698eb8f6a0106c585e61a193f4d626
Julia Brosowski <Julia-Patricia.Brosowski@telekom.de>
2023-08-05 03:06:57 +0200

    form ready

--- src/app/pages/sign-up/sign-up.component.ts
+++ src/app/pages/sign-up/sign-up.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Signup } from 'src/app/models/signup.model';
 
 @Component({
   selector: 'app-sign-up',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./sign-up.component.scss']
 })
 export class SignUpComponent {
+  model: Signup = new Signup("", "", "", "")
+  passwordRepeat: string = ""
+
+  isPasswordMatch(): boolean {
+    return this.model.password === this.passwordRepeat;
+  }
+
+  onSubmit() {
+    console.log(this.model, this.passwordRepeat)
+  }
 
 }
